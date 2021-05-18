75594e22a802ebc8fcbefdd23bdadb08fb4f4baf
suryasai chowdary <suryasaichowdary023@gmail.com>
2021-05-18 10:56:57 +0530

    initial commit

---
+++ Basic server with mongoDB templete.js
@@ -0,0 +1,28 @@
+//jshint esversion:6
+
+const express = require("express");
+const ejs = require("ejs");
+const mongoose = require('mongoose');
+
+
+const app = express();
+
+
+app.set('view engine', 'ejs');
+
+app.use(express.urlencoded({
+  extended: true
+}));
+app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost:27017/wikiDB",{useNewUrlParser:true});
+
+const articleSchema = {
+    name: String,
+    content: String
+};
+
+const Article = mongoose.model("Article", articleSchema);
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
+});
